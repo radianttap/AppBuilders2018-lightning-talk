19e2f1232ee32856b68505c53774776a22e50126
Aleksandar Vacic <av@codeaplus.com>
2018-04-13 20:54:26 +0200

    Added some helpful extensions

---
+++ Vendor/UIKit/UIView-Extensions.swift
@@ -0,0 +1,64 @@
+//
+//  UIView-Extensions.swift
+//  Radiant Tap Essentials
+//
+//  Copyright © 2016 Radiant Tap
+//  MIT License · http://choosealicense.com/licenses/mit/
+//
+
+import UIKit
+
+extension UIView {
+	var top: CGFloat {
+		return frame.minY
+	}
+	var bottom: CGFloat {
+		return frame.maxY
+	}
+	var left: CGFloat {
+		return frame.minX
+	}
+	var right: CGFloat {
+		return frame.maxX
+	}
+
+	//	Methods
+
+	func animateAlpha(to alpha: CGFloat, duration: TimeInterval = 0.3) {
+		UIView.animate(withDuration: duration) {
+			[unowned self] in
+			self.alpha = alpha
+		}
+	}
+
+	func animateLayout(duration: TimeInterval = 0.3, include: (() -> Void)? = nil, completion: ((Bool) -> Void)? = nil) {
+		UIView.animate(withDuration: duration, animations: {
+			[unowned self] in
+			if let include = include { include() }
+			self.layoutIfNeeded()
+			}, completion: completion)
+	}
+
+	func springLayout(duration: TimeInterval = 0.3, include: (() -> Void)? = nil, completion: ((Bool) -> Void)? = nil) {
+		UIView.animate(withDuration: duration,
+		               delay: 0,
+		               usingSpringWithDamping: 0.96,
+		               initialSpringVelocity: 20,
+		               options: .allowAnimatedContent,
+		               animations: {
+						[unowned self] in
+						if let include = include { include() }
+						self.layoutIfNeeded()
+			}, completion: completion)
+	}
+
+	func mask(corners: UIRectCorner = [], cornerRadius: CGFloat = 4) {
+		let maskPath = UIBezierPath(roundedRect: bounds,
+		                            byRoundingCorners: corners,
+		                            cornerRadii: CGSize(width: cornerRadius, height: cornerRadius))
+		let maskLayer = CAShapeLayer()
+		maskLayer.frame = bounds
+		maskLayer.path = maskPath.cgPath
+		layer.mask = maskLayer
+	}
+}
