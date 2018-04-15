423cdb7fd59a802fe3ba0a21a5fc974deb53ee0b
Aleksandar Vacic <av@codeaplus.com>
2018-04-15 16:27:11 +0200

    Added Wait state view

---
+++ AppBuilders2018-demo2/WaitStateView.swift
@@ -0,0 +1,12 @@
+//
+//  WaitStateView.swift
+//  AppBuilders2018-demo
+//
+//  Created by Aleksandar Vacić on 4/13/18.
+//  Copyright © 2018 Radiant Tap. All rights reserved.
+//
+
+import UIKit
+
+final class WaitStateView: UIView, NibLoadableFinalView {
+}
